83a5012a3e08bd3d8c4bfeb91ad72d93eb29a6f0
mhy2011 <mhy2011@163.com>
2016-12-30 09:26:39 +0800

    增加http转向https

--- spring-boot-03/src/main/java/com/mhy/spring/boot/SpringBoot03Application.java
+++ spring-boot-03/src/main/java/com/mhy/spring/boot/SpringBoot03Application.java
@@ -1,12 +1,50 @@
 package com.mhy.spring.boot;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBoot03Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot03Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBoot03Application.class, args);
+  }
+
+  @Bean
+  public EmbeddedServletContainerFactory servletContainerFactory(){
+    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+      @Override
+      protected void postProcessContext(Context context) {
+        super.postProcessContext(context);
+        SecurityConstraint securityConstraint = new SecurityConstraint();
+        securityConstraint.setUserConstraint("CONFIDENTIAL");
+        SecurityCollection collection = new SecurityCollection();
+        collection.addPattern("/*");
+        securityConstraint.addCollection(collection);
+        context.addConstraint(securityConstraint);
+      }
+
+    };
+    tomcat.addAdditionalTomcatConnectors(httpConnector());
+    return tomcat;
+  }
+
+  private Connector httpConnector() {
+    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    connector.setScheme("http");
+    connector.setPort(8080);//用8080端口来供http访问
+    connector.setSecure(false);
+    connector.setRedirectPort(8443);//自动重定向到8443端口
+    return connector;
+  }
+
 }
